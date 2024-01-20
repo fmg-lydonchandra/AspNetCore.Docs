56724686d24f853d0b2cf03110cf083e61832995
Lydon Chandra <lydon.chandra@fmgl.com.au>
2024-01-21 03:13:52 +0800

    use https to proxy grpc to charles

--- aspnetcore/tutorials/grpc/grpc-start/sample/sample7/GrpcGreeterClient/Program.cs
+++ aspnetcore/tutorials/grpc/grpc-start/sample/sample7/GrpcGreeterClient/Program.cs
@@ -1,16 +1,37 @@
-﻿#region snippet2
+﻿using System.Net;
 using System.Threading.Tasks;
 using Grpc.Net.Client;
 using GrpcGreeterClient;
 
-#region snippet
-// The port number must match the port of the gRPC server.
-using var channel = GrpcChannel.ForAddress("https://localhost:7042");
+var proxy = new WebProxy("http://127.0.0.1:8888");//charles
+// var proxy = new WebProxy("http://127.0.0.1:8866"); //fiddler
+var httpClientHandler = new HttpClientHandler()
+{
+    Proxy = proxy,
+    UseProxy = true,
+    // UseProxy = false,
+    ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
+};
+
+var httpClient = new HttpClient(httpClientHandler)
+{
+    DefaultVersionPolicy = HttpVersionPolicy.RequestVersionOrLower,
+    DefaultRequestVersion = HttpVersion.Version20
+};
+// proxy smoke test
+// httpClient.BaseAddress = new Uri("http://www.google.com");
+// var result = httpClient.GetStringAsync("/").GetAwaiter().GetResult();
+
+// Grpc must run as HTTPS so it is proxied through Charles
+var channel = GrpcChannel.ForAddress("https://127.0.0.1:5000", new GrpcChannelOptions()
+{
+    HttpClient = httpClient
+});
+
 var client = new Greeter.GreeterClient(channel);
+
 var reply = await client.SayHelloAsync(
                   new HelloRequest { Name = "GreeterClient" });
 Console.WriteLine("Greeting: " + reply.Message);
 Console.WriteLine("Press any key to exit...");
 Console.ReadKey();
-#endregion
-#endregion
